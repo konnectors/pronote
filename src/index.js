857bc355c8960e5781f783ea300f014799083e28
Vince Linise <vincelinise@gmail.com>
2024-07-17 14:26:58 +0200

    refactor: Update dependencies and start function to authenticate with educonnect and toutatice

--- src/index.js
+++ src/index.js
@@ -1,46 +1,19 @@
 // Importation des fonctions de cozy-konnector-libs
-const {
-  BaseKonnector,
-  log,
-  signin,
-  requestFactory
-} = require('cozy-konnector-libs')
-const Browser = require('zombie')
+const { BaseKonnector, log } = require('cozy-konnector-libs')
+const pronote = require('pronote-api-maintained')
 
 module.exports = new BaseKonnector(start)
 
-const rq = requestFactory({
-  jar: true, // handle the cookies like a browser
-  json: false, // do not try to parse the result as a json document
-  cheerio: true // automatically parse the result with [cheerio](https://github.com/cheeriojs/cheerio)
-})
-
 // Fonction start qui va être exportée
-async function start(fields, cozyParameters) {
-  // step 1.
-  log('info', 'Authenticating ...')
-  authenticate(fields.login, fields.password)
-  log('info', 'Successfully logged in')
-}
+async function start(fields) {
+  const { pronote_url: url, login: username, password } = fields
+  const session = await pronote.login(url, username, password, 'toutatice')
 
-async function authenticate(pronote_url, username, password) {
-  const educ = `https://educonnect.education.gouv.fr/idp/profile/SAML2/Redirect/SSO?execution=e1s1`
-  const ttc = `https://www.toutatice.fr/portail/auth/pagemarker/3/portal/default/bureau-eleve-lyc-2020?init-state=true&redirect=true`
-
-  const browser = new Browser({
-    runScripts: false
+  const qr = await pronote.request(session, 'JetonAppliMobile', {
+    donnees: {
+      code: '1234'
+    }
   })
-  await browser.visit(educ)
 
-  await browser.visit(ttc, async () => {
-    await browser.choose('input[value="eleve-1"]')
-    await browser.pressButton('#eleve button', async () => {
-      await browser.fill('j_username', 'v.linise')
-      await browser.fill('j_password', 'NaomiVince20032007!')
-      browser.runScripts = true
-      await browser.pressButton('#bouton_valider', async () => {
-        console.log(browser.html())
-      })
-    })
-  })
+  log('QR code:', qr)
 }
