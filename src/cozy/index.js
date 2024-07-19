33435344c374d69ea33d61b25aae3b5c3c5d5f82
Vince Linise <vincelinise@gmail.com>
2024-07-19 12:20:44 +0200

    build(tests): :triangular_flag_on_post: Removed cozy_test since it is not necessary anymore

--- src/cozy/index.js
+++ src/cozy/index.js
@@ -1,15 +1,9 @@
-const TESTS_ENABLED = false
-
 const identity = require('./pronote/identity')
 const timetable = require('./pronote/timetable')
 const homeworks = require('./pronote/homeworks')
 const grades = require('./pronote/grades')
 const presence = require('./pronote/presence')
 
-const test_timetable = require('./tests/test_timetable')
-const test_homeworks = require('./tests/test_homeworks')
-const test_grades = require('./tests/test_grades')
-const test_presence = require('./tests/test_presence')
 const { log } = require('cozy-konnector-libs')
 
 async function cozy_save(type, pronote, fields, options = {}) {
@@ -35,32 +29,6 @@ async function cozy_save(type, pronote, fields, options = {}) {
   }
 }
 
-async function cozy_test(type, pronote, fields, options = {}) {
-  try {
-    if (!TESTS_ENABLED) {
-      return true
-    }
-
-    log('info', '🤔 Testing ' + type)
-
-    switch (type) {
-      case 'timetable':
-        return test_timetable(pronote, fields, options)
-      case 'homeworks':
-        return test_homeworks(pronote, fields, options)
-      case 'grades':
-        return test_grades(pronote, fields, options)
-      case 'presence':
-        return test_presence(pronote, fields, options)
-      default:
-        return false
-    }
-  } catch (err) {
-    throw new Error(err)
-  }
-}
-
 module.exports = {
-  cozy_save,
-  cozy_test
+  cozy_save
 }
