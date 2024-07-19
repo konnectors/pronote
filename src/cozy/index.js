6d8ca467c25243159b4d0d2ed9cab652581e9c29
Vince Linise <vincelinise@gmail.com>
2024-07-19 12:29:19 +0200

    refactor(cozy): Refactor cozy_save function to use a handler object

--- src/cozy/index.js
+++ src/cozy/index.js
@@ -3,29 +3,28 @@ const timetable = require('./pronote/timetable')
 const homeworks = require('./pronote/homeworks')
 const grades = require('./pronote/grades')
 const presence = require('./pronote/presence')
-
 const { log } = require('cozy-konnector-libs')
+
+const handlers = {
+  identity,
+  timetable,
+  homeworks,
+  grades,
+  presence
+}
 
 async function cozy_save(type, pronote, fields, options = {}) {
   try {
-    log('info', '🔁 Saving ' + type)
+    log('info', `🔁 Saving ${type}`)
 
-    switch (type) {
-      case 'identity':
-        return identity(pronote, fields, options)
-      case 'timetable':
-        return timetable(pronote, fields, options)
-      case 'homeworks':
-        return homeworks(pronote, fields, options)
-      case 'grades':
-        return grades(pronote, fields, options)
-      case 'presence':
-        return presence(pronote, fields, options)
-      default:
-        return false
+    const handler = handlers[type]
+    if (handler) {
+      return handler(pronote, fields, options)
     }
+
+    return false
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err.message)
   }
 }
 
