35dd8b50360bfd309a6cd10bfc0de9e411cfb3a1
doubleface <christophe@cozycloud.cc>
2024-11-13 11:51:45 +0100

    publish: feat: Pronote design for the URL form
    generated from commit b11e62fe3af0b02956b5d20b298c82ed712ad44c

--- main.js
+++ main.js
@@ -5847,12 +5847,19 @@ const UUID = uuid()
 monkeyPatch(UUID)
 
 class PronoteContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_MODULE_0__.ContentScript {
-  async ensureAuthenticated({ account }) {
+  async ensureAuthenticated({ account, trigger }) {
     this.log('info', '🤖 ensureAuthenticated')
+    const isLastJobError =
+      trigger?.current_state?.last_failure >
+      trigger?.current_state?.last_success
+    this.log('debug', 'isLastJobError: ' + isLastJobError)
+    const lastJobError = trigger?.current_state?.last_error
+    this.log('debug', 'lastJobError: ' + lastJobError)
 
     await this.setWorkerState({ incognito: true })
     let url = account?.data?.url
-    if (!url) {
+    this.log('debug', 'url: ' + url)
+    if (!url || (isLastJobError && lastJobError === 'LOGIN_FAILED')) {
       await this.setWorkerState({ visible: true })
       await this.goto(
         'https://demo.index-education.net/pronote/mobile.eleve.html'
@@ -5860,61 +5867,61 @@ class PronoteContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORT
       await this.waitForElementInWorker('nav')
       url = await this.evaluateInWorker(getUrlFromUser)
       await this.setWorkerState({ visible: false })
-    }
 
-    await this.goto(
-      url + '/infoMobileApp.json?id=0D264427-EEFC-4810-A9E9-346942A862A4'
-    )
-    await new Promise(resolve => window.setTimeout(resolve, 2000))
-    await this.evaluateInWorker(function (UUID) {
-      const PRONOTE_COOKIE_EXPIRED = new Date(0).toUTCString()
-      const PRONOTE_COOKIE_VALIDATION_EXPIRES = new Date(
-        new Date().getTime() + 5 * 60 * 1000
-      ).toUTCString()
-      const PRONOTE_COOKIE_LANGUAGE_EXPIRES = new Date(
-        new Date().getTime() + 365 * 24 * 60 * 60 * 1000
-      ).toUTCString()
-      const json = JSON.parse(document.body.innerText)
-      const lJetonCas = !!json && !!json.CAS && json.CAS.jetonCAS
-      document.cookie = `appliMobile=; expires=${PRONOTE_COOKIE_EXPIRED}`
+      await this.goto(
+        url + '/infoMobileApp.json?id=0D264427-EEFC-4810-A9E9-346942A862A4'
+      )
+      await new Promise(resolve => window.setTimeout(resolve, 2000))
+      await this.evaluateInWorker(function (UUID) {
+        const PRONOTE_COOKIE_EXPIRED = new Date(0).toUTCString()
+        const PRONOTE_COOKIE_VALIDATION_EXPIRES = new Date(
+          new Date().getTime() + 5 * 60 * 1000
+        ).toUTCString()
+        const PRONOTE_COOKIE_LANGUAGE_EXPIRES = new Date(
+          new Date().getTime() + 365 * 24 * 60 * 60 * 1000
+        ).toUTCString()
+        const json = JSON.parse(document.body.innerText)
+        const lJetonCas = !!json && !!json.CAS && json.CAS.jetonCAS
+        document.cookie = `appliMobile=; expires=${PRONOTE_COOKIE_EXPIRED}`
 
-      if (lJetonCas) {
-        document.cookie = `validationAppliMobile=${lJetonCas}; expires=${PRONOTE_COOKIE_VALIDATION_EXPIRES}`
-        document.cookie = `uuidAppliMobile=${UUID}; expires=${PRONOTE_COOKIE_VALIDATION_EXPIRES}`
-        document.cookie = `ielang=1036; expires=${PRONOTE_COOKIE_LANGUAGE_EXPIRES}`
+        if (lJetonCas) {
+          document.cookie = `validationAppliMobile=${lJetonCas}; expires=${PRONOTE_COOKIE_VALIDATION_EXPIRES}`
+          document.cookie = `uuidAppliMobile=${UUID}; expires=${PRONOTE_COOKIE_VALIDATION_EXPIRES}`
+          document.cookie = `ielang=1036; expires=${PRONOTE_COOKIE_LANGUAGE_EXPIRES}`
+        }
+      }, UUID)
+      await this.goto(`${url}/mobile.eleve.html?fd=1`)
+
+      await this.setWorkerState({ visible: true })
+      await this.runInWorkerUntilTrue({
+        method: 'waitForLoginState'
+      })
+      await this.setWorkerState({ visible: false })
+      const loginState = await this.evaluateInWorker(() => window.loginState)
+
+      const loginTokenParams = {
+        url,
+        kind: 6,
+        login: loginState.login,
+        token: loginState.mdp,
+        deviceUUID: UUID
       }
-    }, UUID)
-    await this.goto(`${url}/mobile.eleve.html?fd=1`)
+      this.store = loginTokenParams
 
-    await this.setWorkerState({ visible: true })
-    await this.runInWorkerUntilTrue({
-      method: 'waitForLoginState'
-    })
-    await this.setWorkerState({ visible: false })
-    const loginState = await this.evaluateInWorker(() => window.loginState)
-
-    const loginTokenParams = {
-      url,
-      kind: 6,
-      login: loginState.login,
-      token: loginState.mdp,
-      deviceUUID: UUID
-    }
-    this.store = loginTokenParams
-
-    await this.evaluateInWorker(loginTokenParams => {
-      document.body.innerHTML = `<pre>
+      await this.evaluateInWorker(loginTokenParams => {
+        document.body.innerHTML = `<pre>
     ${JSON.stringify(loginTokenParams, null, 2)}
     </pre>`
-    }, loginTokenParams)
+      }, loginTokenParams)
+    } else {
+      this.store = account?.data
+    }
 
-    // TODO intercepter les identifiants ?
     return true
   }
 
   async ensureNotAuthenticated() {
     // always true if incognito mode
-    // TODO incognito mode
     return true
   }
 
@@ -5969,6 +5976,7 @@ connector
   .catch(err => {
     log.warn(err)
   })
+
 function getUrlFromUser() {
   document.querySelector('nav').remove()
   document.querySelector('form').remove()
